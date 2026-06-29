1971428950ebe683443dec4554da8f1de1249356
Jeffrey Ghinn <jeffreyghinn@outlook.com>
2026-06-29 13:30:50 -0400

    Correct hide window

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -5,6 +5,7 @@ using Microsoft.UI.Xaml.Data;
 using Microsoft.UI.Xaml.Input;
 using Microsoft.UI.Xaml.Media;
 using Microsoft.UI.Xaml.Navigation;
+using Microsoft.UI.Windowing;
 using System;
 using System.Collections.Generic;
 using System.IO;
@@ -12,8 +13,10 @@ using System.Linq;
 using System.Runtime.InteropServices.WindowsRuntime;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
+using WinRT.Interop;
 using KeepAwake11.Services;
 using KeepAwake11.ViewModels;
+using Microsoft.UI;
 
 namespace KeepAwake11;
 
@@ -21,6 +24,7 @@ public sealed partial class MainWindow : Window
 {
 
     private  readonly MainViewModel _viewModel = new();
+    private bool _isInitialized;
     public MainWindow()
     {
         this.InitializeComponent();
@@ -30,13 +34,22 @@ public sealed partial class MainWindow : Window
 
     private async void MainWindow_Loaded(object sender, RoutedEventArgs e)
     {
+        if (_isInitialized) return;
+        _isInitialized = true;
+        
         await _viewModel.InitializeAsync();
 
         KeepAwakeToggle.IsOn = _viewModel.Settings.KeepComputerAwake;
 
         StatusText.Text = _viewModel.Settings.KeepComputerAwake
-            ? "Keep Awake: ON"
-            : "Keep Awake: OFF";
+            ? "Keep Awake: ACTIVE"
+            : "Keep Awake: INACTIVE";
+
+        var hWnd = WindowNative.GetWindowHandle(this);
+        var windowId = Win32Interop.GetWindowIdFromWindow(hWnd);
+        var appWindow = AppWindow.GetFromWindowId(windowId);
+
+        appWindow.Hide();
     }
 
     private async void KeepAwakeToggle_Toggled(object sender, RoutedEventArgs e)
